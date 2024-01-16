bf54929df391e5b77becc767b9612246c51fd3b3
Rafael Andeloci Rodrigues Gonçalves <rafaelandeloci67@gmail.com>
2024-01-15 21:24:44 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Post } from './components/Post';
+import { Header } from './components/Header';
+import { SideBar } from './components/SideBar';
+
+import './global.css';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Est eu nostrud magna dolore incididunt Lorem do tempor qui aliqua ad anim excepteur. Voluptate ea occaecat exercitation amet ea mollit ullamco ipsum exercitation deserunt in elit voluptate cillum. Occaecat nisi magna exercitation pariatur proident. Officia esse esse nulla enim laboris aliqua exercitation ex."
+          />
+          <Post author="Gabriel Buzzi" content="Um novo post muito legal" />
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine } from 'phosphor-react';
+
+import styles from './SideBar.module.css';
+
+export function SideBar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=40&w=5006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/RafaelAndeloci.png"
+        />
+
+        <strong>Rafael Andeloci</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu Perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
